9ea43e5a7c4ccc1962888127bb5100b99b9a4b9a
pavan kalyan <pavankalyanmagam@gmail.com>
2025-05-24 20:58:53 -0500

    Initial Commit

---
+++ service-2-persistence/src/test/java/com/code/delta/service2persistence/Service2PersistenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.code.delta.service2persistence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Service2PersistenceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ service-3-orchestrator/src/test/java/com/code/delta/service3orchestrator/Service3OrchestratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.code.delta.service3orchestrator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Service3OrchestratorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
